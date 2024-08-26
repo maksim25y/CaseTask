a59f2b1e5db92c23a2065ba543193dd70760131b
mudan <myudanov223@gmail.com>
2024-08-26 16:52:40 +0300

    initial commit

---
+++ src/main/java/ru/mudan/CaseTaskApplication.java
@@ -0,0 +1,13 @@
+package ru.mudan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaseTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/mudan/CaseTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mudan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
